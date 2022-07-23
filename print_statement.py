4637c22998bde2056625dcaefbcb5d656c9cac75
unnu2320 <109863203+unnu2320@users.noreply.github.com>
2022-07-23 22:45:27 +0530

    Add files via upload

---
+++ print_statement.py
@@ -0,0 +1,34 @@
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+# print(5)
+# print(6)
+# print(7)
+
+# print('hello today is second day')
+# print("hello today is second day")
+
+# print('hello tod"ay is second day')
+# print("hello tod'ay is second day")
+
+# print('hello tod\'ay is second day ')
+# print("hello today is seco\"nd day ")
+
+
+
+
+from turtle import bye
+
+
+# print('''hi
+# bye
+#    python''')
+# 
+# print('''today we are see'ing pri"nt ststement''')
+# print('''hi
+
+# bye''')
+print('python')
+print()
+print('programing')
